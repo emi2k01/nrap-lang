6ca938803478cbafb356046e2fa5cf37ebc46d18
Emilio González <emigr2k1@gmail.com>
2020-02-13 03:06:21 -0600

    Add RuntimeError and minor ParserError edit

--- src/error.rs
+++ src/error.rs
@@ -8,6 +8,7 @@ pub enum LexicalError {
 
 #[derive(Error, Debug)]
 pub enum ParseError {
+    #[error("There's a syntax error in your program")]
     Generic,
 }
 
@@ -17,11 +18,39 @@ pub enum CompileError {
     NoMainFound,
 }
 
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use ParseError as PE;
-        match &self {
-            PE::Generic => write!(f, "There's an error in your program"),
-        }
-    }
+#[derive(Error, Debug, Clone)]
+pub enum RuntimeError {
+    #[error("There was an error: {0}")]
+    Generic(String),
+
+    #[error("Tried to use an invalid expression in «out» argument. «Out» argument must be an identifier")]
+    OutParamNoIdent,
+
+    #[error("Tried to use an invalid expression in index operation. Index must be a number")]
+    IndexNotANumber,
+
+    #[error("Tried to use a non-procedure statement in the top level of the program. \
+            All non-procedure statements must be inside a procedure")]
+    NonProcedureInTopLevel,
+
+    #[error("Tried to use an invalid expression in «if» statement. \
+            «if» conditions must be bool expressions")]
+    IfConditionNotABool,
+
+    #[error("Tried to use an invalid expression in «break if» statement. \
+            «break if» conditions must be bool expressions")]
+    BreakIfConditionNotABool,
+
+    #[error("Tried to use an invalid procedure in a call expression. To use a procedure in a call \
+            statement, it must have only one «out» parameter and it must be the last one")]
+    ProcedureCallExprNotValid,
+
+    #[error("Tried to use an infix operation with invalid types.")]
+    InfixOpWithInvalidTypes,
+
+    #[error("Tried to use a prefix operation with an invalid type")]
+    PrefixOpWithInvalidType,
+
+    #[error("There's no main procedure")]
+    NoMainProcedure,
 }
